eb06c3a41200bc214d362f1f8303f8a648c808f8
abdessamad <lechhababdessamad79@gmail.com>
2025-08-03 18:19:56 +0200

    Save booking data and fix model usage

--- club/admin.py
+++ club/admin.py
@@ -1,6 +1,8 @@
+
 from django.contrib import admin
 from .models import booking
 
 @admin.register(booking)
-class bookingadmin(admin.ModelAdmin):
+class BookingAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'phone', 'city', 'car_name', 'brand', 'timestamp')
+    search_fields = ('full_name', 'car_name', 'brand')
