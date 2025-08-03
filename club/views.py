d6e7d4bd432c03eb878f3cb7686721be1bedc793
abdessamad <lechhababdessamad79@gmail.com>
2025-08-03 19:31:50 +0200

    Save booking data and fix model usage

--- club/views.py
+++ club/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
 from .models import reservation
+reservation.objects.all()
 
 def home(request):
     return render(request, "home.html")
